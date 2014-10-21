03e880ba5d530f597af9cfa46ca0146682503871
Charlie Root <root@server.pobeda.local>
2014-10-21 14:00:48 +0300

    update

--- update-cidr.sh
+++ update-cidr.sh
@@ -2,8 +2,8 @@
 
 
 if [ ! -d /etc/ckf_rules ]
-mkdir /etc/ckf_rules
+then
+    mkdir /etc/ckf_rules
 fi
 
-/usr/bin/fetch -o ./tmp http://www.okean.com/sinokoreacidr.txt 2> err.log
-cat ./tmp | cut -d ' ' -f 1 > /etc/ckf_rules/china_and_korea.cidr
+/usr/bin/fetch -o /dev/stdout http://www.okean.com/sinokoreacidr.txt 2> err.log | cut -d ' ' -f 1 > /etc/ckf_rules/china_and_korea.cidr
